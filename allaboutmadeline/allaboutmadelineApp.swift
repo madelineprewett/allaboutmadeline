b37d90b9f71f661bb2b79c5abd026fb22cea881b
Madeline Prewett <madsprewett@gmail.com>
2023-07-13 13:03:57 -0700

    Initial Commit

---
+++ allaboutmadeline/allaboutmadelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  allaboutmadelineApp.swift
+//  allaboutmadeline
+//
+//  Created by scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct allaboutmadelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
